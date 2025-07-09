be9ef7a74bc2312f9035d0908ff27b2d378d2bd0
MiQ <kokobay2004@gmail.com>
2025-07-09 23:25:19 +0700

    test

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -1,7 +1,1 @@
-### Mô tả thay đổi
-
-- [ ] Đã test?
-- [ ] Có thay đổi logic?
-- [ ] Có ảnh hưởng UI/UX?
-
-### Screenshot hoặc Video (nếu cần)
+You are welcome!!
--- src/app.js
+++ src/app.js
@@ -1,1 +1,2 @@
-console.log("Xin chào thế giới! Edit master");
+console.log("Xin chào thế giới! Edit feature (resolved conflict)");
+console.log("This is a new line for git advance demo.");
